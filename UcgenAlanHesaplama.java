6706b0f329970e8c8eee37d1bd447afbd70c6ab6
theahmetozturk <113602170+theahmetozturk@users.noreply.github.com>
2022-10-05 19:49:44 +0300

    Ucgend Alan Hesaplama

---
+++ UcgenAlanHesaplama.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class UcgenAlanHesaplama {
+
+	public static void main(String[] args) {
+		
+		double a,b,c,u,alan;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Lütfen 1.Kenar Uzunluğunu Giriniz: ");
+		a= input.nextDouble();
+		
+		System.out.println("Lütfen 2.Kenar Uzunluğunu Giriniz: ");
+		b = input.nextDouble();
+		
+		System.out.println("Lütfen 3.Kenar Uzunluğunu Giriniz: ");
+		c = input.nextDouble();
+		
+		u= (a+b+c)/2.0;
+		alan=Math.sqrt(u*(u-a)*(u-b)*(u-c));
+		
+		System.out.println("1.Kenar Uzunluğu: "+a);
+		System.out.println("2.Kenar Uzunluğu: "+b);
+		System.out.println("3.Kenar Uzunluğu: "+c);
+		System.out.println("Üçgenin Alanı: "+alan);
+	
+		
+		
+		
+		
+		
+	}
+
+}
