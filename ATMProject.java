c916a75da6f56ac5399ab0aa1a00f41a8ad9ddba
theahmetozturk <113602170+theahmetozturk@users.noreply.github.com>
2022-12-02 22:53:46 +0300

    project

--- ATMProject.java
+++ ATMProject.java
@@ -6,7 +6,7 @@ public class ATMProject {
 
 	public static void main(String[] args) {
 		String userName, password;
-		int right = 3, balance = 1500, select;
+		int right = 3, balance = 15000, select;
 
 		Scanner inp = new Scanner(System.in);
 
@@ -17,40 +17,41 @@ public class ATMProject {
 			System.out.println("Please enter your PASSWORD");
 			password = inp.nextLine();
 
-			if (userName.equals("user1") && password.equals("12345")) {
+			if (userName.equals("evrem") && password.equals("merve.27")) {
 				System.out.println("Welcome xxx Bank");
+				do {
 
-				System.out.println("1- Deposit Money\n" + "2- Take Money\n" + "3- Check Money\n" + "4- Quit");
-				System.out.println("Please choose your process: ");
-				select = inp.nextInt();
+					System.out.println("1- Deposit Money\n" + "2- Take Money\n" + "3- Check Money\n" + "4- Quit");
+					System.out.println("Please choose your process: ");
+					select = inp.nextInt();
 
-				switch (select) {
-				case 1:
-					System.out.println("Amount of Money : ");
-					int price = inp.nextInt();
-					balance += price;
-					System.out.println("Your balance : " + balance);
-					break;
-				case 2:
-					System.out.println("Amount of Money : ");
-					price = inp.nextInt();
-					if (price > balance) {
-						System.out.println("Not enough money!!");
-					} else {
-						balance -= price;
+					switch (select) {
+					case 1:
+						System.out.println("Amount of Money : ");
+						int price = inp.nextInt();
+						balance += price;
+						System.out.println("Your balance : " + balance);
+						break;
+					case 2:
+						System.out.println("Amount of Money : ");
+						price = inp.nextInt();
+						if (price > balance) {
+							System.out.println("Not enough money!!");
+						} else {
+							balance -= price;
+						}
+					case 3:
+						System.out.println("Your balance : " + balance);
 					}
-				case 3:
-					System.out.println("Your balance : " + balance);
-				case 4:
-					System.out.println("See you again.");
-				}
+				} while (select != 4);
+				System.out.println("See you again.");
 				break;
-		
+
 			} else {
 				right--;
-				System.out.println("Wrong username or password. Please try again!!");
+				System.out.println("Wrong username or password. Please try again!! ");
 				if (right == 0) {
-					System.out.println("Your account blocked. Please contact with bank!!");
+					System.out.println("Your account blocked. Please contact with bank.");
 				} else {
 					System.out.println("Your right : " + right);
 				}
